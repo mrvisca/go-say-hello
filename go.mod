b8fabc008ba87c1539cd0fc4059ec6dae7cbab51
Mr.Visca <62088810+mrvisca@users.noreply.github.com>
2023-08-21 19:47:28 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mrvisca/go-say-hello
+module github.com/mrvisca/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package go_say_hello
 
 // function harus uppercase agar bisa digunakan untuk publik
-func SayHello() string {
-	return "Hai Visca... Dodo miss you..."
+func SayHello(name string) string { //Kondisi major changes dimana function yang sudah di buat di awal harus menambahkan parameter yang membuat semua user yang sudah menggunakan function tersebut harus menyesuaikan
+	return "Hai Visca... Iam " + name
 }
